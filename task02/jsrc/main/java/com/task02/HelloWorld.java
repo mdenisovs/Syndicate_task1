e438662628567640288b071847a762499dd59297
Maksims_Denisovs <maksims_denisovs@epam.com>
2024-05-28 17:12:28 +0300

    task02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,49 +2,44 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.DeploymentRuntime;
-import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 @LambdaHandler(
 		lambdaName = "hello_world",
-		roleName = "hello_world-role",
-		isPublishVersion = true,
-		aliasName = "${lambdas_alias_name}",
-		resourceGroup = "event_group",
-		runtime = DeploymentRuntime.JAVA11,
-		architecture = Architecture.ARM64
+		roleName = "task02-lambda-role",
+		runtime= DeploymentRuntime.JAVA11
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	@Override
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
-		String path = requestEvent.getRequestContext().getHttp().getPath();
-		String method = requestEvent.getRequestContext().getHttp().getMethod();
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		context.getLogger().log("Invocation started: " + getTimeStamp());
 
-		if (path != null && path.equals("/hello")) {
-			APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
-			response.setStatusCode(200);
-			response.setBody("Hello from Lambda");
+		String message = "Hello from Lambda";
 
-			return response;
-		}
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "{\"statusCode\":200, \"message\": \"Hello from Lambda\"}");
 
-		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
-		response.setStatusCode(400);
-		response.setBody(String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
-				path, method));
+		context.getLogger().log(message);
 
-		return response;
+		context.getLogger().log("Invocation completed: " + getTimeStamp());
+		return resultMap;
+	}
+
+	private String getTimeStamp() {
+		return new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
 	}
 }
